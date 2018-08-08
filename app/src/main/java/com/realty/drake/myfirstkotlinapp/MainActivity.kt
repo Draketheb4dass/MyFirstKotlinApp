7d5b41e122bd19a06d597101cd708719ae26e98b
Jephte Drake Colin <drakecolinj@gmail.com>
2018-08-08 18:29:16 -0400

    Shape button changed

--- app/src/main/java/com/realty/drake/myfirstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/realty/drake/myfirstkotlinapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.realty.drake.myfirstkotlinapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat.startActivity
 import android.view.View
-import android.widget.TextView
 import android.widget.Toast
+import com.realty.drake.myfirstkotlinapp.R.id.tvNumber
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,17 +23,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun countMe(view: View){
-        //Get the view
-        val showCountTextView = findViewById<TextView>(R.id.tvNumber)
-
-        //get the value of the textview
-        val countString = showCountTextView.text.toString()
-
-        //convert value to a number and increment it
+        // Get the value of the text view.
+        val countString = tvNumber.text.toString()
+        // Convert value to a number and increment it
         var count: Int = Integer.parseInt(countString)
         count++
+        // Display the new value in the text view.
+        tvNumber.text = count.toString()
+    }
 
-        //display the value
-        showCountTextView.text = count.toString()
+    fun randomMe(view: View) {
+        val randomIntent = Intent(this, SecondActivity::class.java)
+        // Get the current value of the text view.
+        val countString = tvNumber.text.toString()
+
+        // Convert the count to an int
+        val count = Integer.parseInt(countString)
+
+        // Add the count to the extras for the Intent.
+        randomIntent.putExtra(SecondActivity.TOTAL_COUNT, count)
+        startActivity(randomIntent)
     }
 }
